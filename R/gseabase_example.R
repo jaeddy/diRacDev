9b4096caa92eb13452289e31e4415f5eaeaf2f38
jaeddy <james.a.eddy@gmail.com>
2014-10-07 23:12:25 -0700

    more testing, piecing together map function

--- R/gseabase_example.R
+++ R/gseabase_example.R
@@ -1,17 +1,8 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 library(GEOquery)
 library(GSEABase)
 library(hgu95av2)
 library(dplyr)
-=======
-library(GSEABase)
-library(hgu95av2)
->>>>>>> a26c92e3306cb55aca4f989abeb28b209846232e
-=======
-library(GSEABase)
-library(hgu95av2)
->>>>>>> b9ac84f46928c2accee71eefc597608cdcaa1fad
 
 # Expression data for 500 features and 26 samples
 data(sample.ExpressionSet)
@@ -20,38 +11,46 @@ data(sample.ExpressionSet)
 exprs <- sample.ExpressionSet[201:250, ]
 
 # Create gene set collection
-<<<<<<< HEAD
-<<<<<<< HEAD
 biocartaFile <- "c2.cp.biocarta.v4.0.symbols.gmt"
-fileAddress <- paste0("http://www.broadinstitute.org/gsea/msigdb/download_file",
+# fileAddress <- paste0("http://www.broadinstitute.org/gsea/msigdb/download_file",
                       ".jsp?filePath=/resources/msigdb/4.0/", biocartaFile)
-download.file(fileAddress, paste0("data/", biocartaFile), method = "curl")
+# download.file(fileAddress, paste0("data/", biocartaFile), method = "curl")
 
 bcgsc <- getGmt(paste0("data/", biocartaFile),
                 collectionType = BroadCollection(category = "c2"),
                 geneIdType = SymbolIdentifier())
 bcgsc[2]
 
-features <- fData(exprs)
-
-egsc <- GeneSetCollection(exprs, idType = "missing", setType = BroadCollection())
-
-library(KEGG.db)
-lst <- head(as.list(KEGGEXTID2PATHID))
-gsc <- GeneSetCollection(mapply(function(geneIds, keggId) {
-    GeneSet(geneIds, geneIdType=EntrezIdentifier(),
-            collectionType=KEGGCollection(keggId),
-            setName=keggId)
-}, lst, names(lst)))
-
-geneSymbols <- featureData(exprs) %>%
+fData(sample.ExpressionSet) <- featureData(sample.ExpressionSet) %>%
     row.names() %>%
-    getSYMBOL("hgu95av2")
-=======
-gsc <- GeneSetCollection(exprs, setType = BroadCollection())
-source("http://bioconductor.org/biocLite.R")
->>>>>>> a26c92e3306cb55aca4f989abeb28b209846232e
-=======
-gsc <- GeneSetCollection(exprs, setType = BroadCollection())
-source("http://bioconductor.org/biocLite.R")
->>>>>>> b9ac84f46928c2accee71eefc597608cdcaa1fad
+    getSYMBOL("hgu95av2") %>%
+    as.data.frame() %>%
+    select(symbol = starts_with("featureData"))
+
+f <- fData(sample.ExpressionSet)
+
+allGenes <- unique(as.character(unlist(geneIds(bcgsc))))
+bcGenes <- geneSymbols[geneSymbols %in% allGenes]
+
+map_expression_to_set <- function(gs, es) {
+    mapIdx <- geneIds(gs) %in% fData(es)$symbol
+}
+
+for (gs in bcgsc) {
+    if (sum(geneIds(gs) %in% geneSymbols) > 1) {
+        print(gs)
+    }
+}
+
+
+gs <- bcgsc[["BIOCARTA_GLYCOLYSIS_PATHWAY"]]
+geneIds(gs) %in% geneSymbols
+sum(geneSymbols %in% geneIds(gs))
+mapIdx <- geneSymbols %in% geneIds(gs)
+
+test <- data.frame(gene = geneSymbols[mapIdx], 
+                   exprs(sample.ExpressionSet[mapIdx]))
+
+test2 <- test %>%
+    group_by(gene) %>%
+    summarise_each(funs(max))
